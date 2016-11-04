6295dd19e83a34dfd90daa688ef9823d358af1b5
martin107 <martinfrances107@hotmail.com>
2016-11-04 18:31:05 +0000

    This patch is the sole work of vaplas ( on Drupal.org ). The string delimting is improved and unwanted new lines are filtered form the results.

--- convertExpectedException.php
+++ convertExpectedException.php
@@ -8,28 +8,38 @@ function processFile($filename) {
   $tree = Parser::parseFile($filename);
   // Iterate over each doc block in the file.
   foreach ($tree->find(Filter::isInstanceOf('\Pharborist\DocCommentNode')) as $node) {
-    /** @var \Pharborist\DocCommentNode $node */
+    /** @var \Pharborist\DocCommentNode $doc_block */
     $doc_block = $node->getDocBlock();
     $class_tags = $doc_block->getTagsByName('expectedException');
     if ($class_tags) {
       $exception_class = $class_tags[0]->getDescription();
       //echo "$exception_class\n";
       $message_tags = $doc_block->getTagsByName('expectedExceptionMessage');
+      // Handle multiple combinations of delimiters.
       if ($message_tags) {
         $message = $message_tags[0]->getDescription();
-        $message = preg_replace("/'/", "\\'", $message);
-        //echo "$message\n\n";
+        if(strpos($message, "'") !== FALSE) {
+          if(strpos($message, '"') !== FALSE) {
+            $message = preg_replace('/"/', '\\"', $message);
+          }
+          $message = ', "' . $message . '"';
+        }
+        else{
+          $message = ", '" . $message . "'";
+        }
       }
       else {
         $message = '';
       }
+
+      $method_class = $node->parent(Filter::isInstanceOf('Pharborist\Objects\ClassMethodNode'));
+
       // Search method class - skip whitespaces and open brackets find the
       // first line of code.
-      $method_class = $node->parent(Filter::isInstanceOf('Pharborist\Objects\ClassMethodNode'));
       // Prepare new statement. ( code - newlines - indent ).
       $indentation = str_repeat(" ", $method_class->getColumnNumber() +  1);
       if ($message) {
-        $statement = "\$this->setExpectedException('" . $exception_class . "', '"  . $message . "');" . PHP_EOL . PHP_EOL. $indentation;
+        $statement = "\$this->setExpectedException('" . $exception_class . "'"  . $message . ");" . PHP_EOL . PHP_EOL. $indentation;
       }
       else {
         $statement = "\$this->setExpectedException('$exception_class');" . PHP_EOL . PHP_EOL . $indentation;
@@ -44,7 +54,10 @@ function processFile($filename) {
         ])
       );
       $first_line_of_code = $lines_of_code->first();
+
       $new_nodes->insertBefore($first_line_of_code);
+
+      // Delete the tags from the doc block.
       /** @var Pharborist\DocCommentNode $doc_comment */
       $doc_comment = $method_class->getDocComment();
       $text = $doc_comment->getText();
@@ -53,7 +66,12 @@ function processFile($filename) {
       $text = preg_replace('/\n\s*\*\s*\n(\s*\*\/)/', "\n$1", $text);
       $doc_comment->setText($text);
       $method_class->setDocComment($doc_comment);
-      file_put_contents($filename, $tree);
+
+      // Phsrborist add a newline that we need to remove.
+      $unfiltered_result = $tree->getText();
+      $result = preg_replace('@\*/\n  (?=\n)@', '*/', $unfiltered_result);
+
+      file_put_contents($filename, $result);
     }
   }
 }
@@ -80,11 +98,6 @@ function recursive($dir)
   closedir($odir);
 }
 
-// This exposed a flaw in pharborist.
-// Unless Drupal core is patched with  DO-NOT_COMMIT-2822837-7.patch the classes
-// in core/modules/system/src/Tests/Installer will cause errors.
-// The following code will iterate over every class in D8 core
-// - please /Users/martin/sites/drupal/core to point to the core directory for your system.
 $dir = '/path/to/drupal.site/core';
 $directory = new \RecursiveDirectoryIterator($dir);
 $directories = [];
@@ -95,7 +108,7 @@ foreach($directories as $dir){
   $pattern = '/^.+Test\.php$/i';
   $regex = new \RegexIterator($iterator, $pattern, \RecursiveRegexIterator::GET_MATCH);
   foreach ($regex as $name => $object) {
-    echo "processing: $name<br>\n";
+    echo "processing: $name\n";
     processFile($name);
   }
 }
